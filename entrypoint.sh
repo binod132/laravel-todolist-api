bd6c5ed79e512015385fa19fb02c12db3a3f9057
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-10 13:31:28 +0545

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,19 @@ php-fpm &
 # Wait for PHP-FPM to start
 sleep 5
 
-# Run the artisan command to generate passport keys
-if [ ! -f storage/oauth-private.key ]; then
+# Check and generate the encryption key if not set
+if [ -z "$APP_KEY" ]; then
+    echo "Generating Laravel encryption key..."
+    php artisan key:generate --force
+fi
+
+# Generate Passport keys if they do not exist
+if [ ! -f /var/www/storage/oauth-private.key ]; then
     echo "Generating Passport keys..."
     php artisan passport:keys
 else
     echo "Passport keys already exist."
 fi
 
-# Bring PHP-FPM to the foreground
+# Bring PHP-FPM to the foreground to keep the container running
 wait
