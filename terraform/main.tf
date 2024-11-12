2418de7f366663913fc31380d8601fc9f99b6db1
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-12 17:28:20 +0545

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,47 +1,53 @@
-## Configure the vSphere Provider
 provider "vsphere" {
-    vsphere_server = "${var.vsphere_server}"
-    user = "${var.vsphere_user}"
-    password = "${var.vsphere_password}"
-    allow_unverified_ssl = true
+  user                  = "root"
+  password              = "Mid@s123"
+  vsphere_server        = "192.168.130.210"
+  allow_unverified_ssl  = true
+  api_timeout           = 10
 }
 
-## Build VM
-data "vsphere_datacenter" "dc" {
-  name = "ha-datacenter"
+data "vsphere_datacenter" "datacenter" {
+  name = "ha-datacenter"  # Change to your ESXi datacenter name
+}
+
+data "vsphere_host" "host" {
+  name          = "localhost"  # Change to your ESXi host name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  name          = "datastore1"  # Name of your datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_resource_pool" "pool" {}
-
-data "vsphere_network" "mgmt_lan" {
-  name          = "VM Network"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+data "vsphere_network" "network" {
+  name          = "VM Network"  # Change to your network name
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "vsphere_virtual_machine" "test2" {
-  name             = "test2"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+resource "vsphere_virtual_machine" "vm" {
+  name             = "ubuntu-vm"  # Name for the VM
+  resource_pool_id = data.vsphere_host.host.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 2  # Set number of CPUs
+  memory           = 4096  # Set memory in MB
+  guest_id         = "ubuntu64Guest"  # Guest OS type
 
-  num_cpus   = 1
-  memory     = 2048
-  wait_for_guest_net_timeout = 0
-  guest_id = "ubuntu64Guest"
-  nested_hv_enabled =true
   network_interface {
-   network_id     = "${data.vsphere_network.mgmt_lan.id}"
-   adapter_type   = "vmxnet3"
+    network_id = data.vsphere_network.network.id
   }
-  
+
   disk {
-   size             = 16
-   label            = "tf"
-   eagerly_scrub    = false
-   thin_provisioned = true
+    label = "Hard Disk 1"
+    size  = 20  # Size in GB
   }
+
+  # Use existing .vmx file located on the datastore
+  vmx_file = "/vmfs/volumes/datastore1/iso/os_template/ubuntu-server-template/ubuntu-22.04.vmx"  # Path to your .vmx file
+
+  state = "poweredon"  # Power on the VM after creation
+}
+
+output "vm_ip" {
+  value = vsphere_virtual_machine.vm.network_interface.0.ipv4_address
 }
