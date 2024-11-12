5f93496de1521bb54ead21bfff8b76da546242e3
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-12 10:47:48 +0545

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,31 +7,44 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+# Specify the ESXi datacenter
 data "vsphere_datacenter" "dc" {
   name = "ha-datacenter"
 }
 
+# Define the datastore on ESXi
 data "vsphere_datastore" "datastore" {
   name          = "datastore1"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# Define the network on ESXi
 data "vsphere_network" "network" {
   name          = "VM Network"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# Define the VM template on ESXi
 data "vsphere_virtual_machine" "template" {
   name          = "ubuntu.vmx"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# Define the folder for the VM
+data "vsphere_folder" "vm_folder" {
+  path          = "vm"  # Root folder for VMs in ESXi
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Create a new VM from the template
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-vm"
+  folder_id        = data.vsphere_folder.vm_folder.id  # Reference to the VM folder
   datastore_id     = data.vsphere_datastore.datastore.id
   num_cpus         = 2
   memory           = 4096
   guest_id         = data.vsphere_virtual_machine.template.guest_id
+
   network_interface {
     network_id   = data.vsphere_network.network.id
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
