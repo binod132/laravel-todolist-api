69d5540dc71d0892c4efd7003e81643d00a0514f
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-12 16:10:04 +0545

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,6 @@ data "vsphere_network" "network" {
 # Define the VM template on ESXi
 data "vsphere_virtual_machine" "template" {
   name          = "ubuntu.vmx"  # Template located under [datastore1] iso/os_template/ubuntu-24.04
-  datastore_id  = data.vsphere_datastore.datastore.id
   folder_path   = "iso/os_template/ubuntu-24.04"  # Ensure the path under the datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
