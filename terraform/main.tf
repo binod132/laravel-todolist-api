6b88bed90aa09aa97189fbcf57327155335e5c71
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-12 16:18:57 +0545

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,78 +1,47 @@
+## Configure the vSphere Provider
 provider "vsphere" {
-  user           = "root"
-  password       = "Mid@s123"
-  vsphere_server = "192.168.130.210"
-
-  # If your server uses a self-signed certificate
-  allow_unverified_ssl = true
+    vsphere_server = "${var.vsphere_server}"
+    user = "${var.vsphere_user}"
+    password = "${var.vsphere_password}"
+    allow_unverified_ssl = true
 }
 
-# Specify the ESXi datacenter
+## Build VM
 data "vsphere_datacenter" "dc" {
   name = "ha-datacenter"
 }
 
-# Define the datastore on ESXi
 data "vsphere_datastore" "datastore" {
   name          = "datastore1"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-# Define the network on ESXi
-data "vsphere_network" "network" {
+data "vsphere_resource_pool" "pool" {}
+
+data "vsphere_network" "mgmt_lan" {
   name          = "VM Network"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-# Define the VM template on ESXi
-data "vsphere_virtual_machine" "template" {
-  name          = "ubuntu.vmx"  # Template located under [datastore1] iso/os_template/ubuntu-24.04
-  folder_path   = "iso/os_template/ubuntu-24.04"  # Ensure the path under the datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
+resource "vsphere_virtual_machine" "test2" {
+  name             = "test2"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-# Define the folder for the VM (use the correct folder path)
-data "vsphere_folder" "vm_folder" {
-  path          = "vm"  # This is commonly used for the root VM folder in ESXi
-}
-
-# Create a new VM from the template
-resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-vm"
-  resource_pool_id = "ha-datacenter/host/localhost/Resources"  # Use root resource pool for ESXi
-  folder           = data.vsphere_folder.vm_folder.path  # Folder where the VM will be created
-  datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 2
-  memory           = 4096
-  guest_id         = data.vsphere_virtual_machine.template.guest_id
-
+  num_cpus   = 1
+  memory     = 2048
+  wait_for_guest_net_timeout = 0
+  guest_id = "ubuntu64Guest"
+  nested_hv_enabled =true
   network_interface {
-    network_id   = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+   network_id     = "${data.vsphere_network.mgmt_lan.id}"
+   adapter_type   = "vmxnet3"
   }
 
   disk {
-    label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks[0].size
-    eagerly_scrub    = false
-    thin_provisioned = true
-  }
-
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-
-    customize {
-      linux_options {
-        host_name = "terraform-vm"
-        domain    = "tv.local"
-      }
-
-      network_interface {
-        ipv4_address = "192.168.130.112"
-        ipv4_netmask = 24
-      }
-
-      ipv4_gateway = "192.168.130.1"
-    }
+   size             = 16
+   name             = "test2.vmdk"
+   eagerly_scrub    = false
+   thin_provisioned = true
   }
 }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+vsphere_server = "192.168.130.210"
+vsphere_user = "root"
+vsphere_password = "Mid@s123"
