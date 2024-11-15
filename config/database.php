746598e68a6e52aaf20ac5b02fd8c513c397f5c8
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-15 13:10:28 +0545

    fixed

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => app(\App\Services\VaultService::class)->getDbPassword(),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
