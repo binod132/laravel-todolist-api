1c2670a5a794d8f09db379f4d5d14c685de90be6
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2024-11-17 13:29:18 +0545

    fixed

--- app/Services/VaultService.php
+++ app/Services/VaultService.php
@@ -2,7 +2,13 @@
 
 namespace App\Services;
 
+use Vault\AuthenticationStrategies\AppRoleAuthenticationStrategy;
+use Vault\AuthenticationStrategies\UserPassAuthenticationStrategy;
+use Vault\AuthenticationStrategies\TokenAuthenticationStrategy;
 use Vault\Client;
+use Laminas\Diactoros\RequestFactory;
+use Laminas\Diactoros\StreamFactory;
+use Laminas\Diactoros\Uri;
 use Vault\Exceptions\VaultException;
 
 class VaultService
@@ -12,17 +18,32 @@ class VaultService
     public function __construct()
     {
         // Initialize Vault client
-        $this->vault = new Client([
-            'base_uri' => env('VAULT_ADDR'), // Set the Vault address in .env file
-            'token' => env('VAULT_TOKEN')    // Set the Vault token in .env file
-        ]);
+        $uri = new Uri(env('VAULT_ADDR')); // Get Vault address from .env
+        $client = new Client(
+            $uri,
+            new \AlexTartan\GuzzlePsr18Adapter\Client(), // PSR-18 Adapter for Guzzle
+            new RequestFactory(),
+            new StreamFactory()
+        );
+
+        // Set namespace if required
+        $client->setNamespace('Vault); // Optional, if you're using a Vault namespace
+
+        // Choose authentication method (for example, Token)
+        $authenticated = $client
+            ->setAuthenticationStrategy(new TokenAuthenticationStrategy(env('VAULT_TOKEN'))) // Vault token from .env
+            ->authenticate();
+
+        // Assign the client to the class property
+        $this->vault = $client;
     }
 
     public function getDbPassword()
     {
         try {
+            // Read the secret from Vault
             $secret = $this->vault->read('secret/db_credentials');
-            return $secret['data']['POSTGRES_PASSWORD'];
+            return $secret['data']['POSTGRES_PASSWORD']; // Return the password from Vault
         } catch (VaultException $e) {
             throw new \Exception('Failed to fetch the DB password from Vault');
         }
